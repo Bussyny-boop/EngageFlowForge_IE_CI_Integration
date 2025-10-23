2e5776324a018e2d7a5b5cfe1f04f0f1b63a20f7
Bussyny-boop <bussyny@gmail.com>
2025-10-23 08:07:29 -0700

    Update Main.java

--- src/main/java/com/example/exceljson/Main.java
+++ src/main/java/com/example/exceljson/Main.java
@@ -1,6 +1,6 @@
 package com.example.exceljson;
 
-import jafx.application.Application;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
