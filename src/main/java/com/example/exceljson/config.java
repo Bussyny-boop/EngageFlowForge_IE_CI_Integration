59ba1aa98383851dff6ca8688f273459d3e1b6da
Bussyny-boop <bussyny@gmail.com>
2025-10-23 08:24:33 -0700

    Update config.java

--- src/main/java/com/example/exceljson/config.java
+++ src/main/java/com/example/exceljson/config.java
@@ -1,13 +1,17 @@
 package com.example.exceljson;
 
 public class Config {
-    public String outputVersion;
-    public boolean attachAllUnitsIfNoMatch;
+    public String outputVersion = "1.0";
+    public boolean attachAllUnitsIfNoMatch = true;
     public Sheets sheets = new Sheets();
     public MappingAliases aliases = new MappingAliases();
     public RecipientParsing recipientParsing = new RecipientParsing();
 
     public static class RecipientParsing {
-        public String functionalRoleRegex;
+        public String functionalRoleRegex = "^(?i)(vassign:.*|.*\\[room\\].*)$";
+    }
+
+    public static Config defaultConfig() {
+        return new Config();
     }
 }
